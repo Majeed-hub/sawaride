8dd057f0e37c82fe8b9a3346d0ddad71d45c5fac
Majeed-hub <majeedbkl65@gmail.com>
2024-10-22 19:25:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import SawaLogo from "./assets/images/SawaLogo.png";
 
 function App() {
   return (
     <div className="App">
+      {/* Iframe background */}
+      <iframe
+        src="https://my.spline.design/3dcitynavigationcopy-78a5326874435a5b665b6111aa66396e/"
+        frameBorder="0"
+        className="App-iframe"
+        title="3D City"
+      ></iframe>
+
+      {/* Foreground content */}
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={SawaLogo} className="App-logo" alt="logo" />
+      </header>
+      <header className="App-body">
+        <h1>Welcome to open mobility</h1>
+       
+        <h2>Welcome to open mobility</h2>
+        
+        <h2>Welcome to open mobility</h2>
+        
       </header>
     </div>
   );
